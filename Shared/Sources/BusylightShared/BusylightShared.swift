6b9a18fe08211344d583ffb1a8d7dad1e706390a
tuanama <jaraujo@skyones.co>
2026-03-08 16:57:24 -0500

    Remove old multi-platform folders
    - Remove Shared/ (old shared framework)
    - Remove iOS/ (old iOS app)
    - Remove watchOS/ (old watchOS app)
    - Remove macOS/ (old macOS folder structure)
    - Remove old Busylight.xcodeproj
    - Remove old BusylightTests and BusylightUITests
    
    Project now uses single 'Busylight macOS' folder structure

--- Shared/Sources/BusylightShared/BusylightShared.swift
+++
@@ -1,8 +0,0 @@
-// BusylightShared - Shared code across all platforms
-
-// Export all models
-@_exported import Foundation
-@_exported import SwiftData
-
-// Version
-public let BusylightSharedVersion = "1.0.0"
