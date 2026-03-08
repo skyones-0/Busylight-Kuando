9aebf287b02b5762f98f37856c374f704b87fe1f
tuanama <jaraujo@skyones.co>
2026-03-08 06:17:44 -0500

    Fix build errors and clean up project
    - Fix test file to use XCTest instead of Testing framework
    - Remove @testable import that was causing module dependency error
    - Clean up deleted multiplatform directories from project
    - Verify build succeeds for main target

--- BusylightTests/BusylightTests.swift
+++ BusylightTests/BusylightTests.swift
@@ -5,13 +5,28 @@
 //  Created by Jose Araujo on 20/02/26.
 //
 
-import Testing
-@testable import Busylight
+import XCTest
 
-struct BusylightTests {
+final class BusylightTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        XCTAssertTrue(true)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 }
